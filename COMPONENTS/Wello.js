5dd09455b44f8b3ed9516f510f29444f32244d43
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-08-22 20:41:53 +0530

    Add files via upload

---
+++ COMPONENTS/Wello.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Wello = () => {
+    return React.createElement('div',{id:'Hello',className:'dummyClass'},React.createElement('h1',null,'Hello JSX 2'))
+} 
+export default Wello;
